612fa2a32e1dcd72791faf6395ecd542c12ae3a2
Franflii <franlopezpineda@gmail.com>
2024-10-15 13:44:03 +0200

    ActPag8

---
+++ Ud3/src/ud3/ActPag8.java
@@ -0,0 +1,19 @@
+package ud3;
+
+public class ActPag8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 0;
+		
+		while(a <= 50) {
+			System.out.println(a);
+			a = a + 2;
+		}
+		
+		
+	}
+
+	}
+
